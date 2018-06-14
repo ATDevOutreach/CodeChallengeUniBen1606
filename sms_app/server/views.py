ee5f29dea68e27c71b718a0428f97786cfa861a5
teenoh <valentino@grit.systems>
2018-06-15 00:38:47 +0100

    bug fix in reply_message

--- sms_app/server/views.py
+++ sms_app/server/views.py
@@ -6,20 +6,24 @@ from django.views.decorators.csrf import csrf_exempt
 import africastalking
 
 
-# Create your views here.
 
 username = os.environ["USERNAME"]
 api_key = os.environ["API_KEY"] 
+
+#initialize api
 africastalking.initialize(username, api_key)
 
 sms = africastalking.SMS
 
+#we aren't using a form, so csrf isn't needed
 @csrf_exempt
 def index(request):
     if request.method == "POST":
-        sender = request.POST["from"]
+        sender = request.POST["from"] 
         message = request.POST["text"]
         
-        response = sms.send("I am a fisherman. I sleep all day and work all night!", recipients = [sender,], sender_id="9798")
+        reply_message = "I am a fisherman. I sleep all day and work all night!" 
+
+        sms.send(reply_message, recipients = [sender,], sender_id="9798")
     
     return HttpResponse(status=200)
